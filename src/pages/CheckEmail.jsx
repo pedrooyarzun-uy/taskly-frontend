d28a58be8d65193f3d95810afd62bdb0ee3a648c
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2025-06-26 20:21:25 -0300

    Text for check email in english 😆

--- src/pages/CheckEmail.jsx
+++ src/pages/CheckEmail.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 export const CheckEmail = () => {
   return (
-    <div className='text-center'>
-        <p className='text-2xl font-bold'>Tu registro fue exitoso 🤩!</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, aliquam aliquid. Reiciendis saepe corporis excepturi autem aspernatur. Earum ut debitis quod distinctio, facilis, unde ea, ullam quia quis corporis adipisci.</p>
+    <div className='text-center mt-4'>
+      <p className='text-2xl font-bold'>Your registration was successful 🤩!</p>
+      <p>We have sent an email to the address you provided so you can verify it.</p>
     </div>
+
   )
 }
