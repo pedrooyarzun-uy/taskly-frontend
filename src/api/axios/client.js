64520bf935aac8fb0e9fbcfe2708c2076a80c6d3
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2025-06-27 13:45:05 -0300

    Fix for env variables

--- src/api/axios/client.js
+++ src/api/axios/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL:  import.meta.env.VITE_BACKEND_URL,
+    baseURL:  import.meta.env.VITE_BACKEND_URL + "/api",
     timeout: 10000,
     headers: {
         "Content-Type": "application/json"
