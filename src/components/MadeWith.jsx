24aff2c391a3e664d1acc7fef52220d777917d59
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2025-08-24 13:58:22 -0300

    Addedd href on made with

--- src/components/MadeWith.jsx
+++ src/components/MadeWith.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const MadeWith = () => {
   return (
     <div className='bg-white text-black rounded-md shadow-lg shadow-gray-300 w-auto'>
-        <p className='p-4 text-center'>Made with ❤️ by pedrooyarzun-uy</p>
+        <p className='p-4 text-center'>Made with ❤️ by <a href='https://github.com/pedrooyarzun-uy' className='underline' target='_blank'>pedrooyarzun-uy</a></p>
     </div>
   )
 }
