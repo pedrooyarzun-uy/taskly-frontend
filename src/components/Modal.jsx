a12f546845a4e0b3f317b5562acd40603590d5dc
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2025-08-31 22:12:46 -0300

    Added fix on category task render

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,7 +5,7 @@ export const Modal = ({children, isOpen, onClose}) => {
   return (
     <>
       {isOpen && 
-        <div className='fixed inset-0 bg-black/25 backdrop-blur-sm flex justify-center items-center'>
+        <div className='fixed inset-0 bg-black/25 backdrop-blur-sm flex justify-center items-center z-[9999]'>
 
           <div className='bg-white rounded-md flex flex-col justify-center items-center w-[80vw] sm:w-[50vw] md:w-[50vw] lg:w-[25vw]'>
             <div className='flex flex-row justify-end w-full pt-2 pl-2 pr-2'>
